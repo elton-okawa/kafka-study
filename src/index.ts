14f5fc58142930123bc29ef2a134efbffb838ff7
Elton Okawa <elton.okawa@gmail.com>
2024-07-27 10:07:14 -0300

    feat: boilerplate

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+});
+
+async function main() {
+  fastify.get('/', async function handler(request, reply) {
+    return { hello: 'world' };
+  });
+
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+main();
